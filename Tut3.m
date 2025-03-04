0fda4a8f8608ae9a2424670830c727fa8d3684b6
George Milford <georgemilford@Georges-MacBook-Pro.local>
2025-03-04 11:47:20 +0000

    commit files milestone

--- Tut3.m
+++ Tut3.m
@@ -1,5 +1,8 @@
-function noteX = note(A, keynum, dur);
+function noteX = Tut3(A, keynum, dur)
 
+A = 25;
+keynum = 49;
+dur = 2;
 Fs = 11025;
 Ts = 1/Fs;
 A4 = 440;
@@ -9,5 +12,5 @@ n = keynum - ref_key;
 Freq = A4*2^ (n/12);
 Time = 0:Ts:dur;
 
-noteX= A*sin (2*pi*freq*Time);
+noteX= A*sin (2*pi*Freq*Time);
 end
